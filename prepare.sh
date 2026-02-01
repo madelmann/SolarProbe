9235aad75ee50930ac58a1609f8f93725dfd6deb
madelmann <no-reply@github.com>
2026-02-01 22:59:38 +0100

    fixed permissions

--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,8 @@ echo "Preparing environment..."
 
 BASE=$(pwd)
 
-read -p "Server: " SERVER
-
 export BASE
-export SERVER
 
-envsubst < ${BASE}/client/client.conf.tmp   > ${BASE}/client/client.conf
 envsubst < ${BASE}/config/base.conf.tmp     > ${BASE}/config/base.conf
 envsubst < ${BASE}/config/probes.conf.tmp   > ${BASE}/config/probes.conf
 
