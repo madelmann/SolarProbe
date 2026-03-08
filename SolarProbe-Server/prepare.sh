aadf50b4c756008c3cfc44f0899ea1cf3866eef2
madelmann <no-reply@github.com>
2026-03-08 21:04:36 +0100

    automatically set permissions to 777 for webserver target folders

--- SolarProbe-Server/prepare.sh
+++ SolarProbe-Server/prepare.sh
@@ -11,5 +11,8 @@ envsubst < ${BASE}/config/probes.conf.tmp   > ${BASE}/config/probes.conf
 
 cp -r ${BASE}/templates/gifs/ www/
 
+sudo chmod -R 777 ${BASE}/nodes
+sudo chmod -R 777 ${BASE}/tmp
+
 echo "Done preparing environment."
 
